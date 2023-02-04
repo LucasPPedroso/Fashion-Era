b934fa1f28aa372f1efbf6f74227e1f2e474ce92
iabolfazl83 <iabolfazl83@yahoo.com>
2023-02-04 21:34:14 +0330

    Fashion Era Added: Bag Page.

---
+++ assets/js/products.js
@@ -0,0 +1,132 @@
+const products = [
+    {
+        id: 1,
+        images: ['first-slider-slider1.png'],
+        category: "Kids",
+        title: "Yellow T-Shirt",
+        price: {
+            new: 180,
+            old: 300,
+        },
+        off: "40%",
+        rate: 4,
+        sale: 2324,
+        qty: 0,
+        total: 0
+    },
+
+    {
+        id: 2,
+        images: ['first-slider-slider2.png'],
+        category: "Women",
+        title: "Ethnic Kurti",
+        price: {
+            new: 220,
+            old: 250,
+        },
+        off: "12%",
+        rate: 4,
+        sale: 2324,
+        qty: 0,
+        total: 0
+    },
+
+    {
+        id: 3,
+        images: ['first-slider-slider3.png'],
+        category: "Women",
+        title: "Kurtis, Tunics & Tops",
+        price: {
+            new: 280,
+            old: 350,
+        },
+        off: "20%",
+        rate: 4,
+        sale: 2324,
+        qty: 0,
+        total: 0
+    },
+
+    {
+        id: 4,
+        images: ['first-slider-slider4.png'],
+        category: "Men",
+        title: "Round Neck T-shirt",
+        price: {
+            new: 120,
+            old: 150,
+        },
+        off: "20%",
+        rate: 4,
+        sale: 2324,
+        qty: 0,
+        total: 0
+    },
+
+    {
+        id: 5,
+        images: ['kid model.png'],
+        category: "Kids",
+        title: "Kurti Full Sleev",
+        price: {
+            new: 200,
+            old: 300,
+        },
+        off: "33%",
+        rate: 4,
+        sale: 2324,
+        qty: 0,
+        total: 0
+    },
+
+    {
+        id: 6,
+        images: ['hindi women.png'],
+        category: "Women",
+        title: "Silk Saree - Printied ",
+        price: {
+            new: 320,
+            old: 410,
+        },
+        off: "22%",
+        rate: 4,
+        sale: 2324,
+        qty: 0,
+        total: 0
+    },
+
+    {
+        id: 7,
+        images: ['men model uk.png'],
+        category: "Men",
+        title: "Multi Purpose Jacket",
+        price: {
+            new: 510,
+            old: 705,
+        },
+        off: "28%",
+        rate: 4,
+        sale: 2324,
+        qty: 0,
+        total: 0
+    },
+
+    {
+        id: 8,
+        images: ['men model polo.png'],
+        category: "Men",
+        title: "Jacket",
+        price: {
+            new: 400,
+            old: 480,
+        },
+        off: "17%",
+        rate: 4,
+        sale: 2324,
+        qty: 0,
+        total: 0
+    },
+
+];
+
+export default products;
