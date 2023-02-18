4e1829789875105f9f5322d1c1028ca24c1c036f
iabolfazl83 <iabolfazl83@yahoo.com>
2023-02-18 14:22:36 +0330

    Fashion Era Update: fixed card items badge in card page (synced with number of items in card).

--- assets/js/cart.js
+++ assets/js/cart.js
@@ -1,12 +1,18 @@
 const plus = document.querySelector(".plus");
 const minus = document.querySelector(".minus");
 const input = document.querySelector(".qty-input");
+const cardBadge = document.querySelectorAll(".card-number-badge")
+
+document.addEventListener("DOMContentLoaded", () => {
+    cardBadge.forEach(item => {
+        item.innerHTML = JSON.parse(localStorage.getItem("final Products")).length
+    })
+})
 
 plus.addEventListener("click", () => {
     input.value++;
     minus.style.opacity = "1";
 })
-
 minus.addEventListener("click", () => {
     input.value--;
     minus.style.opacity = "1";
@@ -15,3 +21,4 @@ minus.addEventListener("click", () => {
         minus.style.opacity = "0.4";
     }
 })
+
